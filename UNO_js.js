7addd58ddb7d4685ee446b993d3e05ea627d5b0f
Vascure <jana.troppenauer@gmx.at>
2024-03-28 15:21:56 +0100

    errors only

--- UNO_js.js
+++ UNO_js.js
@@ -9,47 +9,48 @@ let gameID;
 // Modal Dialogue for player names
 
 let myModal = new bootstrap.Modal(document.getElementById('playerNames'));
-myModal.show();
 
-document.getElementById('playerForm').addEventListener('submit', async function (evt)
- {
-    evt.preventDefault();
+document.getElementById('playerForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
 
 
     });
 
-
+console.log('hello!')
     //Creating Players
 
     function createPlayer() {
 
         let error = 0;
 
-        for (let i = 1; i<=4; i++)
-        {
-            const playerName = document.getElementById(`playerName${i}`).value;
-
-            if(!playerName[i])
-            {
-                alert('Please enter all player names.');
-                 return;
-            }
-
-            if(playerNamesArray.includes(playerName))
-            {
-                alert('Please enter all player names.');
-                 return;
-            }
-
-            playerNamesArray.push(playerName);
-        }
+        var player1 = document.getElementById('player1').value.trim();
+        var player2 = document.getElementById('player2').value.trim();
+        var player3 = document.getElementById('player3').value.trim();
+        var player4 = document.getElementById('player4').value.trim();
+  
+        if (player1 === '' || player2 === '' || player3 === '' || player4 === '') {
+            showError('Please type in all of your friends names');
+            return;
+          }
+          
+          if (player1 === player2 || player1 === player3 || player1 === player4 || player2 === player3 || player2 === player4 || player3 === player4) {
+            showError('Everyone is an individual snowflake! So use other names.');
+            return;
+          }
+          
+          console.log('Player 1:', player1);
+          console.log('Player 2:', player2);
+          console.log('Player 3:', player3);
+          console.log('Player 4:', player4);
+        };
 
         if ( error == 0)
         {
             myModal.hide();
+            console.log(playerNamesArray)
         }
-    }
+    
 
     //start Game
 
@@ -57,7 +58,7 @@ document.getElementById('playerForm').addEventListener('submit', async function 
 
         const response = await fetch("https://nowaunoweb.azurewebsites.net/api/game/start", {
         method: 'POST',
-        body: JSON.stringify(playerNamesArray),
+        body: JSON.stringify(playerNames),
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         }
@@ -74,4 +75,9 @@ document.getElementById('playerForm').addEventListener('submit', async function 
     }
     }
 
-    startGame();
+    //Path for cards
+    const CardsPath = "./uno_karten_originaldesign/";
+
+    function buildCardDeck(color, number) {
+        return `${CardsPath}${color + number}.png`;
+    }
